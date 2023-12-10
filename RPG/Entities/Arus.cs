d00c63b0028ba3de2b02e8dad22e95931aa61c59
Fulvio Cezar Canducci Dias <fulviocanducci@hotmail.com>
2023-12-10 20:18:15 -0300

    Arus Class

--- RPG/Entities/Arus.cs
+++ RPG/Entities/Arus.cs
@@ -2,7 +2,20 @@
 {
    public class Arus
    {
+      public Arus(string? name, int level, string? heroType)
+      {
+         Name = name;
+         Level = level;
+         HeroType = heroType;
+      }
+
       public string? Name { get; set; }
       public int Level { get; set; }
+      public string? HeroType { get; set; }
+
+      public override string ToString()
+      {
+         return $"{Name} {Level} {HeroType}";
+      }
    }
 }
--- RPG/Program.cs
+++ RPG/Program.cs
@@ -1,10 +1,17 @@
-﻿namespace RPG
+﻿using RPG.Entities;
+
+namespace RPG
 {
    internal class Program
    {
       static void Main(string[] args)
       {
-         Console.WriteLine("Hello, World!");
+         Arus hero = new("Arus", 2, "Knight");
+         Arus oponent = new("Maleficus", 99, "Devil");
+
+
+         Console.WriteLine(hero);
+         Console.WriteLine(oponent);
       }
    }
 }

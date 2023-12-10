4df885ec67f90998482390d8de9139a379c8dc47
Fulvio Cezar Canducci Dias <fulviocanducci@hotmail.com>
2023-12-10 20:08:41 -0300

    Update !!!

---
+++ RPG/Entities/Arus.cs
@@ -0,0 +1,8 @@
+﻿namespace RPG.Entities
+{
+   public class Arus
+   {
+      public string? Name { get; set; }
+      public int Level { get; set; }
+   }
+}
--- RPG/Program.cs
+++ RPG/Program.cs
@@ -1,4 +1,4 @@
-﻿namespace ConsoleApp1
+﻿namespace RPG
 {
    internal class Program
    {
